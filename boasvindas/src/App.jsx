b21d4bb2c8fdee52f28729469b1cb8589ac25c08
marianafernandesdemenezes <marianafernandesdemenezes34@gmail.com>
2025-04-08 12:23:56 -0300

    08/04

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,20 @@
+import BoasVindas from "./exemplo1"
+
+function App() {
+  
+  return (
+  <div>
+   <h1>
+    Boas Vindas ao React com JavaScript</h1> 
+    <p>
+      Este é o arquivo app.jsx que será o nosso ponto de partida para o desenvolvimento de aplicação React utilizando JavaScript.
+    </p>
+    <BoasVindas nome="mariana" />
+    <BoasVindas nome="rafaella" />
+    <BoasVindas nome="emanuele" />
+    <BoasVindas nome="giovana" /> 
+  </div> 
+  )
+}
+
+export default App
